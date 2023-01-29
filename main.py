7cc5663e34613081c42f3cc7bc5ebedbed27e567
hiro16131104 <hiro931104@gmail.com>
2023-01-30 05:12:12 +0900

    apprunnerの設定ファイルを一部変更7

--- main.py
+++ main.py
@@ -364,7 +364,7 @@ def error_handler_others(error):
     }), 500
 
 
-# # Flaskアプリの起動
-# if __name__ == ("__main__"):
-#     # localhost以外からのアクセスを許可
-#     app.run(host="0.0.0.0", port=5000, threaded=True)
+# Flaskアプリの起動
+if __name__ == ("__main__"):
+    # localhost以外からのアクセスを許可
+    app.run(host="0.0.0.0", port=5000, threaded=True)
