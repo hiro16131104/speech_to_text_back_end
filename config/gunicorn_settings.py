b787d189c95a4962d412e539d1fe9cfbe0c6085f
hiro16131104 <hiro931104@gmail.com>
2023-02-06 11:28:03 +0900

    gunicornの設定ファイルを変更

--- config/gunicorn_settings.py
+++ config/gunicorn_settings.py
@@ -1,7 +1,12 @@
 # アプリケーション・サーバー（gunicorn）の設定ファイル
 
+# mainファイルのappインスタンス
 wsgi_app = "main:app"
-bind = "0.0.0.0:5000"
-proc_name = 'infrastructure_flask'
+# ipアドレス+port番号
+# bind = "0.0.0.0:5000"
+# 任意の名称
+proc_name = "infrastructure_flask"
+# ワーカースレッド数
 workers = 2
+# デバッグ時、ソースコードを自動反映
 reload = True
